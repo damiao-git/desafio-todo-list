8aa4a7a1b95f13631b3cd5ed2bedc39aa2af628c
Gaos <damiao.jose@hotmail.com>
2024-12-03 19:38:58 -0300

    Crud finalizado

--- desafio-todo-list-back/app/Http/Controllers/TarefaController.php
+++ desafio-todo-list-back/app/Http/Controllers/TarefaController.php
@@ -14,7 +14,7 @@ class TarefaController extends Controller
         $tarefas = Tarefa::all();
         return response()->json([
             'tarefas' => $tarefas
-        ], 200);
+        ], 200, [], JSON_UNESCAPED_UNICODE);
     }
 
     public function create()
@@ -28,26 +28,37 @@ class TarefaController extends Controller
 
         $tarefa = Tarefa::create($validated);
 
-        if($tarefa){
+        if ($tarefa) {
             return response()->json([
                 'status' => '201',
                 'mensagem' => 'Tarefa criada com sucesso!',
                 'dados' => $tarefa
-            ], 201);
-        }
-        else{
+                , [], JSON_UNESCAPED_UNICODE            ], 201, [], JSON_UNESCAPED_UNICODE);
+        } else {
             return response()->json([
                 'status' => '400',
                 'mensagem' => 'Erro ao criar Tarefa'
             ], 400);
-
         }
     }
 
-    public function show(Tarefa $tarefa)
+    public function show($id)
     {
-        //
+        $tarefa = Tarefa::find($id);
+
+        if ($tarefa) {
+            return response()->json([
+                'status' => 200,
+                'dados' => $tarefa
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => 404,
+                'mensagem' => 'Tarefa não encontrada.'
+            ], 404, [], JSON_UNESCAPED_UNICODE);
+        }
     }
+
 
 
     public function edit(Tarefa $tarefa)
@@ -56,13 +67,43 @@ class TarefaController extends Controller
     }
 
 
-    public function update(Request $request, Tarefa $tarefa)
+    public function update(TarefaValidator $request, Tarefa $tarefa)
     {
-        //
+        $validated = $request->validated();
+
+        if ($tarefa) {
+            $tarefa->update($validated);
+        }
+        return response()->json([
+            'status' => 200,
+            'data' => $tarefa
+        ], 200, [], JSON_UNESCAPED_UNICODE);
     }
 
     public function destroy(Tarefa $tarefa)
     {
-        //
+        $tarefa->delete();
+
+        return response()->json([
+            'status' => '200',
+            'mensagem' => 'Tarefa excluida com sucesso!'
+        ], 200, [], JSON_UNESCAPED_UNICODE);
+    }
+
+    public function filtrarPorSituacao($situacao){
+        $tarefas = Tarefa::where('situacao', $situacao)->get();
+        if(count($tarefas) > 0){
+            return response()->json([
+                'status' => 200,
+                'tarefas'=> $tarefas
+            ],200, [], JSON_UNESCAPED_UNICODE);
+        }
+        else{
+            return response()->json([
+                'status' => 404,
+                'mensagem'=> 'Nenhuma tarefa com o status ' . $situacao
+            ], 404, [], JSON_UNESCAPED_UNICODE);
+
+        }
     }
 }
